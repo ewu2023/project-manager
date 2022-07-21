25b52da87e780fea5501fe4245a6df3e944db79b
Eric Wu <ericwu2019@gmail.com>
2022-07-21 15:25:08 -0400

    Created static version of app

--- pm-app/src/index.js
+++ pm-app/src/index.js
@@ -1,11 +1,84 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+
+class TaskCard extends React.Component {
+    render() {
+        return (
+            <li key={this.props.taskName} class='card'>{this.props.taskName}</li>
+        );
+    }
+}
+
+class TaskList extends React.Component {
+    render() {
+        const tasks = this.props.tasks;
+        const cardElements = tasks.map((task) => {
+            return (
+                <TaskCard taskName={task.name} />
+            );
+        });
+
+        return (
+            <ul class='list'>
+                <h2>{this.props.listName}</h2>
+                {cardElements}
+            </ul>
+        );
+    }
+}
+
+class TaskBoard extends React.Component {
+    render() {
+        const taskLists = this.props.lists;
+        const listElements = taskLists.map((taskList) => {
+            return <TaskList tasks={taskList.tasks} listName={taskList.taskName} />
+        });
+
+        return (
+            <div class='board'>
+                {listElements}
+            </div>
+        );
+    }
+}
+
+const tasks_1 = [
+    {name: 'Task 1'},
+    {name: 'Task 2'}
+];
+
+const tasks_2 = [
+    {name: 'Complete UI'},
+    {name: 'Complete Backend'}
+];
+
+const tasks_3 = [
+    {name: 'Deliver Pizza'}
+];
+
+const task_lists = [
+    {
+        taskName: 'To Do',
+        tasks: tasks_1
+    },
+
+    {
+        taskName: 'In Progress',
+        tasks: tasks_2
+    },
+
+    {
+        taskName: 'Completed',
+        tasks: tasks_3
+    }
+];
 
 class App extends React.Component {
     render() {
         return (
             <div id='App'>
-                <h1>Hello World</h1>
+                <TaskBoard lists={task_lists} />
             </div>
         );
     }
